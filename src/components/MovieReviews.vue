14ce3c02dbcbb97904fdba9c84e8b3b6dea6e425
Erik Lindstrom <elindstrom42@gmail.com>
2021-02-04 15:41:25 -0600

    formatted date for reviews

--- src/components/MovieReviews.vue
+++ src/components/MovieReviews.vue
@@ -12,7 +12,7 @@
           <v-list-item-content>
             <v-list-item-title>{{ review.author }}</v-list-item-title>
             <v-list-item-subtitle>
-              {{ review.created_at }}
+              {{ formatDate(review.created_at) }}
             </v-list-item-subtitle>
             <div class="mt-2">{{ review.content }}</div>
           </v-list-item-content>
@@ -27,8 +27,9 @@
 
 <script>
 import { mapState } from "vuex";
-
+import { dateMixin } from "../mixins/movieMixins";
 export default {
+  mixins: [dateMixin],
   computed: {
     ...mapState(["reviews", "movieDetails"]),
   },
